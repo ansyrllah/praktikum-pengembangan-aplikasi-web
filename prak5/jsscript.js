29543dbcbfb3e3178feb52fb3b8b4050a9d3f123
Ansyarullah <ansyarullahgo@gmail.com>
2023-10-18 22:33:17 +0700

    Add files via upload

---
+++ prak5/jsscript.js
@@ -0,0 +1,8 @@
+console.log('Hello world!!');
+console.log(9 + 1);
+console.log(9 + 3);
+console.log(9 + 5);
+
+let greetings = prompt('Masukkan nama: ');
+alert('Selamat datang ' + greetings);
+alert('Jalan');
